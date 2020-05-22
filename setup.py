9534ec71fb0a4d6612103509fac4541de8ae16f4
韩志超 <hanzhichao@secoo.com>
2020-05-22 16:50:57 +0800

    fix package name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email='superhin@126.com',
     description='Easy parser for parse template $variable',
     long_description=read_file('README.md'),
-    long_description_content_type="text/markdown",  # 新参数
+    # long_description_content_type="text/markdown",  # 新参数
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
@@ -28,13 +28,13 @@ setup(
     license="MIT license",
     include_package_data=True,
     keywords=[
-        'parser', 'template render', 'dollar parser',
+        'parserz', 'parser', 'dollar parser',
     ],
     name='parserz',
     packages=find_packages(include=['parserz']),
     setup_requires=setup_requirements,
     url='https://github.com/hanzhichao/parserz',
-    version='0.01',
+    version='0.12',
     zip_safe=True,
     install_requires=['pyyaml', 'jsonpath']
 )
